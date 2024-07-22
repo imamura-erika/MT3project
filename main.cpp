68e4bc4e5f77855714d06e442c2ee20e8c20181c
今村エリカ <k023g1045@g.neec.ac.jp>
2024-07-22 20:56:02 +0900

    [00_04確認課題] 3次元での回転

--- main.cpp
+++ main.cpp
@@ -1,6 +1,69 @@
 #include <Novice.h>
+#include <Matrix4x4.h>
+#include <Vector3.h>
+#include <cmath>
 
-const char kWindowTitle[] = "GC2B_03_イマムラ_エリカ_タイトル";
+const char kWindowTitle[] = "GC2B_03_イマムラ_エリカ_MT_00_04_確認課題";
+
+// Multiply
+Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2) {
+	Matrix4x4 result = {};
+	for (int i = 0; i < 4; ++i) {
+		for (int j = 0; j < 4; ++j) {
+			result.m[i][j] = 0;
+			for (int k = 0; k < 4; ++k) {
+				result.m[i][j] += m1.m[i][k] * m2.m[k][j];
+			}
+		}
+	}
+	return result;
+}
+
+// 1. X軸回転行列
+Matrix4x4 MakeRotateXMatrix(float radian) {
+	float cosTheta = std::cos(radian);
+	float sinTheta = std::sin(radian);
+	return { 1.0f, 0.0f, 0.0f, 0.0f,
+			0.0f, cosTheta, sinTheta, 0.0f,
+			0.0f, -sinTheta, cosTheta, 0.0f,
+			0.0f, 0.0f, 0.0f, 1.0f };
+}
+
+// 2. Y軸回転行列
+Matrix4x4 MakeRotateYMatrix(float radian) {
+	float cosTheta = std::cos(radian);
+	float sinTheta = std::sin(radian);
+	return { cosTheta, 0.0f, -sinTheta, 0.0f,
+			0.0f, 1.0f, 0.0f, 0.0f,
+			sinTheta, 0.0f, cosTheta, 0.0f,
+			0.0f, 0.0f, 0.0f, 1.0f };
+}
+
+// 3. Z軸回転行列
+Matrix4x4 MakeRotateZMatrix(float radian) {
+	float cosTheta = std::cos(radian);
+	float sinTheta = std::sin(radian);
+	return { cosTheta, sinTheta, 0.0f, 0.0f,
+			-sinTheta, cosTheta, 0.0f, 0.0f,
+			0.0f, 0.0f, 1.0f, 0.0f,
+			0.0f, 0.0f, 0.0f, 1.0f };
+}
+
+// 数値表示用
+static const int kRowHeight = 20;
+static const int kColumnWidth = 60;
+void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix, const char* label) {
+	// 文字列の表示
+	Novice::ScreenPrintf(x, y - kRowHeight, "%s", label);
+
+	// 行列の要素を表示
+	for (int row = 0; row < 4; ++row) {
+		for (int column = 0; column < 4; ++column) {
+			Novice::ScreenPrintf(
+				x + column * kColumnWidth, y + row * kRowHeight, "%6.02f", matrix.m[row][column]);
+		}
+	}
+}
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
@@ -25,6 +88,16 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓更新処理ここから
 		///
 
+		Vector3 rotate{ 0.4f,1.43f,-0.8f };
+		// 1. X軸回転行列
+		Matrix4x4 rotateXMatrix = MakeRotateXMatrix(rotate.x);
+		// 2. Y回転行列
+		Matrix4x4 rotateYMatrix = MakeRotateYMatrix(rotate.y);
+		// 3. Z軸回転行列
+		Matrix4x4 rotateZMatrix = MakeRotateZMatrix(rotate.z);
+
+		Matrix4x4 rotateXYZMatrix = Multiply(rotateXMatrix, Multiply(rotateYMatrix, rotateZMatrix));
+
 		///
 		/// ↑更新処理ここまで
 		///
@@ -32,6 +105,12 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
+
+		// 数値表示
+		MatrixScreenPrintf(0, kRowHeight, rotateXMatrix, "rotateXMatrix");
+		MatrixScreenPrintf(0, kRowHeight * 6, rotateYMatrix, "rotateYMatrix");
+		MatrixScreenPrintf(0, kRowHeight * 6 * 2, rotateZMatrix, "rotateZMatrix");
+		MatrixScreenPrintf(0, kRowHeight * 6 * 3, rotateXYZMatrix, "rotateXYZMatrix");
 
 		///
 		/// ↑描画処理ここまで
